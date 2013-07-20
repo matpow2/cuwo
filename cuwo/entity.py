f30fdebc450e79954a393b3d531280fcc3ad7473
Mathias Kaerlev <matpow2@gmail.com>
2013-07-20 12:54:27 +0200

    PEP8 for entity.py as well

--- cuwo/entity.py
+++ cuwo/entity.py
@@ -1,17 +1,17 @@
 # Copyright (c) Mathias Kaerlev 2013.
 #
 # This file is part of cuwo.
-# 
+#
 # cuwo is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # cuwo is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with cuwo.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -20,28 +20,22 @@ from cuwo.common import is_bit_set
 
 
 class ItemUpgrade(Loader):
-    def reset(self):
-        pass
-    
     def read(self, reader):
         self.x = reader.read_int8()
         self.y = reader.read_int8()
         self.z = reader.read_int8()
         self.material = reader.read_int8()
         self.level = reader.read_uint32()
-    
+
     def write(self, writer):
         writer.write_int8(self.x)
         writer.write_int8(self.y)
         writer.write_int8(self.z)
         writer.write_int8(self.material)
         writer.write_uint32(self.level)
-    
+
 
 class ItemData(Loader):
-    def reset(self):
-        pass
-    
     def read(self, reader):
         self.type = reader.read_uint8()
         self.sub_type = reader.read_uint8()
@@ -60,7 +54,7 @@ class ItemData(Loader):
             new_item.read(reader)
             self.items.append(new_item)
         self.upgrade_count = reader.read_uint32()
-    
+
     def write(self, writer):
         writer.write_uint8(self.type)
         writer.write_uint8(self.sub_type)
@@ -76,12 +70,9 @@ class ItemData(Loader):
         for item in self.items:
             item.write(writer)
         writer.write_uint32(self.upgrade_count)
-    
+
 
 class AppearanceData(Loader):
-    def reset(self):
-        pass
-    
     def read(self, reader):
         self.not_used_1 = reader.read_uint8()
         self.not_used_2 = reader.read_uint8()
@@ -124,7 +115,7 @@ class AppearanceData(Loader):
         self.foot_offset = reader.read_vec3()
         self.back_offset = reader.read_vec3()
         self.wing_offset = reader.read_vec3()
-    
+
     def write(self, writer):
         writer.write_uint8(self.not_used_1)
         writer.write_uint8(self.not_used_2)
@@ -167,13 +158,12 @@ class AppearanceData(Loader):
         writer.write_vec3(self.foot_offset)
         writer.write_vec3(self.back_offset)
         writer.write_vec3(self.wing_offset)
-    
+
+
 FLAGS_1_HOSTILE = 0x20
 
+
 class EntityData(Loader):
-    def reset(self):
-        pass
-    
     def read(self, reader):
         self.x = reader.read_int64()
         self.y = reader.read_int64()
@@ -262,7 +252,7 @@ class EntityData(Loader):
             self.skills.append(reader.read_uint32())
         self.mana_cubes = reader.read_uint32()
         self.name = reader.read_ascii(16)
-    
+
     def write(self, writer):
         writer.write_int64(self.x)
         writer.write_int64(self.y)
@@ -345,7 +335,8 @@ class EntityData(Loader):
             writer.write_uint32(item)
         writer.write_uint32(self.mana_cubes)
         writer.write_ascii(self.name, 16)
-    
+
+
 def read_masked_data(entity, reader):
     mask = reader.read_uint64()
     if is_bit_set(mask, 0):
@@ -471,6 +462,7 @@ def read_masked_data(entity, reader):
     if is_bit_set(mask, 47):
         entity.mana_cubes = reader.read_uint32()
 
+
 def get_masked_size(mask):
     size = 0
     if is_bit_set(mask, 0):
@@ -570,6 +562,7 @@ def get_masked_size(mask):
     if is_bit_set(mask, 47):
         size += 4
 
+
 def write_masked_data(entity, writer):
     writer.write_uint64(0x0000FFFFFFFFFFFF)
     writer.write_int64(entity.x)
@@ -647,105 +640,105 @@ def write_masked_data(entity, writer):
     writer.write_uint32(entity.mana_cubes)
 
 SOUNDS = {
-    0 : 'hit',
-    1 : 'blade1',
-    2 : 'blade2',
-    3 : 'long-blade1',
-    4 : 'long-blade2',
-    5 : 'hit1',
-    6 : 'hit2',
-    7 : 'punch1',
-    8 : 'punch2',
-    9 : 'hit-arrow',
-    10 : 'hit-arrow-critical',
-    11 : 'smash1',
-    12 : 'slam-ground',
-    13 : 'smash-hit2',
-    14 : 'smash-jump',
-    15 : 'swing',
-    16 : 'shield-swing',
-    17 : 'swing-slow',
-    18 : 'swing-slow2',
-    19 : 'arrow-destroy',
-    20 : 'blade1',
-    21 : 'punch2',
-    22 : 'salvo2',
-    23 : 'sword-hit03',
-    24 : 'block',
-    25 : 'shield-slam',
-    26 : 'roll',
-    27 : 'destroy2',
-    28 : 'cry',
-    29 : 'levelup2',
-    30 : 'missioncomplete',
-    31 : 'water-splash01',
-    32 : 'step2',
-    33 : 'step-water',
-    34 : 'step-water2',
-    35 : 'step-water3',
-    36 : 'channel2',
-    37 : 'channel-hit',
-    38 : 'fireball',
-    39 : 'fire-hit',
-    40 : 'magic02',
-    41 : 'watersplash',
-    42 : 'watersplash-hit',
-    43 : 'lich-scream',
-    44 : 'drink2',
-    45 : 'pickup',
-    46 : 'disenchant2',
-    47 : 'upgrade2',
-    48 : 'swirl',
-    49 : 'human-voice01',
-    50 : 'human-voice02',
-    51 : 'gate',
-    52 : 'spike-trap',
-    53 : 'fire-trap',
-    54 : 'lever',
-    55 : 'charge2',
-    56 : 'magic02',
-    57 : 'drop',
-    58 : 'drop-coin',
-    59 : 'drop-item',
-    60 : 'male-groan',
-    61 : 'female-groan',
-    62 : 'male-groan',
-    63 : 'female-groan',
-    64 : 'goblin-male-groan',
-    65 : 'goblin-female-groan',
-    66 : 'lizard-male-groan',
-    67 : 'lizard-female-groan',
-    68 : 'dwarf-male-groan',
-    69 : 'dwarf-female-groan',
-    70 : 'orc-male-groan',
-    71 : 'orc-female-groan',
-    72 : 'undead-male-groan',
-    73 : 'undead-female-groan',
-    74 : 'frogman-male-groan',
-    75 : 'frogman-female-groan',
-    76 : 'monster-groan',
-    77 : 'troll-groan',
-    78 : 'mole-groan',
-    79 : 'slime-groan',
-    80 : 'zombie-groan',
-    81 : 'Explosion',
-    82 : 'punch2',
-    83 : 'menu-open2',
-    84 : 'menu-close2',
-    85 : 'menu-select',
-    86 : 'menu-tab',
-    87 : 'menu-grab-item',
-    88 : 'menu-drop-item',
-    89 : 'craft',
-    90 : 'craft-proc',
-    91 : 'absorb',
-    92 : 'manashield',
-    93 : 'bulwark',
-    94 : 'bird1',
-    95 : 'bird2',
-    96 : 'bird3',
-    97 : 'cricket1',
-    98 : 'cricket2',
-    99 : 'owl1',
-    100 : 'owl2'
+    0: 'hit',
+    1: 'blade1',
+    2: 'blade2',
+    3: 'long-blade1',
+    4: 'long-blade2',
+    5: 'hit1',
+    6: 'hit2',
+    7: 'punch1',
+    8: 'punch2',
+    9: 'hit-arrow',
+    10: 'hit-arrow-critical',
+    11: 'smash1',
+    12: 'slam-ground',
+    13: 'smash-hit2',
+    14: 'smash-jump',
+    15: 'swing',
+    16: 'shield-swing',
+    17: 'swing-slow',
+    18: 'swing-slow2',
+    19: 'arrow-destroy',
+    20: 'blade1',
+    21: 'punch2',
+    22: 'salvo2',
+    23: 'sword-hit03',
+    24: 'block',
+    25: 'shield-slam',
+    26: 'roll',
+    27: 'destroy2',
+    28: 'cry',
+    29: 'levelup2',
+    30: 'missioncomplete',
+    31: 'water-splash01',
+    32: 'step2',
+    33: 'step-water',
+    34: 'step-water2',
+    35: 'step-water3',
+    36: 'channel2',
+    37: 'channel-hit',
+    38: 'fireball',
+    39: 'fire-hit',
+    40: 'magic02',
+    41: 'watersplash',
+    42: 'watersplash-hit',
+    43: 'lich-scream',
+    44: 'drink2',
+    45: 'pickup',
+    46: 'disenchant2',
+    47: 'upgrade2',
+    48: 'swirl',
+    49: 'human-voice01',
+    50: 'human-voice02',
+    51: 'gate',
+    52: 'spike-trap',
+    53: 'fire-trap',
+    54: 'lever',
+    55: 'charge2',
+    56: 'magic02',
+    57: 'drop',
+    58: 'drop-coin',
+    59: 'drop-item',
+    60: 'male-groan',
+    61: 'female-groan',
+    62: 'male-groan',
+    63: 'female-groan',
+    64: 'goblin-male-groan',
+    65: 'goblin-female-groan',
+    66: 'lizard-male-groan',
+    67: 'lizard-female-groan',
+    68: 'dwarf-male-groan',
+    69: 'dwarf-female-groan',
+    70: 'orc-male-groan',
+    71: 'orc-female-groan',
+    72: 'undead-male-groan',
+    73: 'undead-female-groan',
+    74: 'frogman-male-groan',
+    75: 'frogman-female-groan',
+    76: 'monster-groan',
+    77: 'troll-groan',
+    78: 'mole-groan',
+    79: 'slime-groan',
+    80: 'zombie-groan',
+    81: 'Explosion',
+    82: 'punch2',
+    83: 'menu-open2',
+    84: 'menu-close2',
+    85: 'menu-select',
+    86: 'menu-tab',
+    87: 'menu-grab-item',
+    88: 'menu-drop-item',
+    89: 'craft',
+    90: 'craft-proc',
+    91: 'absorb',
+    92: 'manashield',
+    93: 'bulwark',
+    94: 'bird1',
+    95: 'bird2',
+    96: 'bird3',
+    97: 'cricket1',
+    98: 'cricket2',
+    99: 'owl1',
+    100: 'owl2'
 }
