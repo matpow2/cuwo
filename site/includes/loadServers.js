972e1142b92e2c69a2f3c0be9f3f517bbd3a0883
Kyle <kyle@ospnet.org>
2013-07-25 22:25:45 -0400

    Implemented the PHP logic entirely in Javascript, removing the dependency on the PHP script for the server list.

--- site/includes/loadServers.js
+++ site/includes/loadServers.js
@@ -1,29 +1,32 @@
+function escapeHTML(text) {
+    return String(text).replace(/&/g, "&amp;")
+               .replace(/</g, "&lt;")
+               .replace(/>/g, "&gt;")
+               .replace(/"/g, "&quot;")
+               .replace(/'/g, "&#039;");
+}
+
 function ajaxRequest() {
-  var xmlhttpRequest;
-  if (window.XMLHttpRequest) {
-    xmlhttpRequest = new XMLHttpRequest();
-  }
-  else if (window.ActiveXObject) {
-    try {
-      xmlhttpRequest = new ActiveXObject("Msxml2.XMLHTTP");
+  $.getJSON('http://mp2.dk/cuwo/servers.json', function(jsonData) {
+    var table = document.getElementById('serverListTable');
+    while(table.hasChildNodes()) {
+      table.removeChild(table.firstChild);
     }
-    catch(e) {
-      try {
-      xmlhttpRequest = new ActiveXObject("Microsoft.XMLHTTP");
-      }
-      catch(e) {}
-    }
-  }
-  else {
-    return false;
-  }        
-        
-  xmlhttpRequest.onreadystatechange = function() {
-    if (xmlhttpRequest.readyState === 4 && xmlhttpRequest.status === 200) {
-      document.getElementById("serverListTable").innerHTML = xmlhttpRequest.responseText;
-      setTimeout('ajaxRequest()', 20000);
-    }
-  }
-  xmlhttpRequest.open('GET', 'includes/serversOnlineTable.php', true);
-  xmlhttpRequest.send();
+
+    $.each(jsonData, function() {
+      var row = table.insertRow();
+      var name = row.insertCell(0);
+      var mode = row.insertCell(1);
+      var players = row.insertCell(2);
+      var ip = row.insertCell(3);
+      var location = row.insertCell(4);
+      name.innerHTML = escapeHTML(this.name);
+      mode.innerHTML = escapeHTML(this.mode);
+      players.innerHTML = escapeHTML(this.players + '/' + this.max);
+      ip.innerHTML = escapeHTML(this.ip);
+      location.innerHTML = '<span class="hidden-desktop hidden-phone hidden-tablet">' + escapeHTML(this.location) + '</span> <img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" class="flag flag-' + escapeHTML(this.location).toLowerCase() + '" alt="' + escapeHTML(this.location) + '" />';
+
+    });
+  });
+  setTimeout('ajaxRequest()', 20000);
 }
