b8324ad8a0206fc4e94bc332e1446e4024572e55
Cody Scott <cs475x@gmail.com>
2013-07-26 16:48:55 -0700

    -Padding on left & right -Moved refresh button -Switched to local copy of logo -Added github link to navigation -Added country flag tooltip -Replaced double quotes with single quotes to be consistent

--- site/js/serverlist.js
+++ site/js/serverlist.js
@@ -14,9 +14,9 @@ function loadServerList() {
       $('<td/>').html(this.mode).appendTo(row);
       $('<td/>').html(this.players + '/' + this.max).appendTo(row);
       $('<td/>').html(this.ip).appendTo(row);
-      
+
       var loc = $('<div/>').html(this.location).html();
-      var image = $('<img/>').attr('src', 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7').attr('alt', loc).addClass('flag flag-' + loc.toLowerCase());
+      var image = $('<img/>').attr('src', 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7').attr('alt', loc).attr('title', loc).addClass('flag flag-' + loc.toLowerCase());
       $('<td/>').append($('<span/>').text(loc).addClass('hidden-desktop hidden-phone hidden-tablet')).append(image).appendTo(row);
       rowHolder.append(row);
     });
