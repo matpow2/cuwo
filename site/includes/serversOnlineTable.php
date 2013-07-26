fc028976f919f52b53ba65f55130e5a2167556d7
Mathias Kaerlev <matpow2@gmail.com>
2013-07-26 03:14:05 +0200

    Fix XSS vuln in serversOnlineTable.php

--- site/includes/serversOnlineTable.php
+++ site/includes/serversOnlineTable.php
@@ -5,8 +5,8 @@ $serverList = json_decode($jsonFileAsString);
 
 foreach($serverList as $cuwoServer): ?>
 <tr>
-<td><?=$cuwoServer->name?></td>
-<td><?=$cuwoServer->mode?></td>
+<td><?=htmlspecialchars($cuwoServer->name)?></td>
+<td><?=htmlspecialchars($cuwoServer->mode)?></td>
 <td><?=$cuwoServer->players?>/<?=$cuwoServer->max?></td>
 <td><?=$cuwoServer->ip?></td>
 <td><span class="hidden-desktop hidden-phone hidden-tablet"><?=$cuwoServer->location?></span><img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" class="flag flag-<?=strtolower($cuwoServer->location)?>" alt="<?=$cuwoServer->location?>" /></td>
