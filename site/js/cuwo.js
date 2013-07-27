bdd70a8347c8eaf343700e73327ad5856c3cc482
Cody Scott <cs475x@gmail.com>
2013-07-26 18:32:33 -0700

    Fix multi-refresh
    -Store reload_action in variable and clearTimeout before resetting
    (oops)
    -Change page title
    -Combine CSS
    -Combine JS
    -Rename CSS, JS, and logo to cuwo.*
    -Prepend 'jquery.' to tablesorter.bootstrap.css

--- site/js/cuwo.js
+++ site/js/cuwo.js
@@ -1,3 +1,22 @@
+$(function() {
+  loadServerList();
+
+  $('#serverlist').tablesorter({
+    theme: 'bootstrap',
+    widthFixed: true,
+    headerTemplate : '{content} {icon}',
+    widgets: [ 'uitheme', 'zebra' ],
+    sortList: [[2,1]]
+  })
+  .tablesorterPager({
+    container: $('.pager'),
+    cssGoto: '.pagenum',
+    output: 'Page {page} of {totalPages} ({startRow}&ndash;{endRow} / {totalRows})',
+    size: 40
+  });
+});
+
+var reload_action;
 function loadServerList() {
   $.getJSON('http://mp2.dk/cuwo/servers.json', function(jsonData) {
     var table = $('#serverlist');
@@ -16,12 +35,15 @@ function loadServerList() {
       var loc = $('<div/>').html(this.location).html();
       var image = $('<img/>').attr('src', 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7').attr('alt', loc).attr('title', loc).addClass('flag flag-' + loc.toLowerCase());
       $('<td/>').append($('<span/>').text(loc).addClass('hidden-desktop hidden-phone hidden-tablet')).append(image).appendTo(row);
+
       rowHolder.append(row);
     });
 
-    if (Object.keys(jsonData).length < 40) $("#pagination").hide(); else $("#pagination").show();
+    if (Object.keys(jsonData).length < 40) $('#pagination').hide(); else $('#pagination').show();
 
-    table.trigger("update", [true]);
-    var reload_action = setTimeout('loadServerList()', 20000);
+    table.trigger('update', [true]);
+
+    clearTimeout(reload_action);
+    reload_action = setTimeout('loadServerList()', 20000);
   });
 }
