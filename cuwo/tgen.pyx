a97e240f12658f4e5cad967cb61409f2f4e23a1c
Mathias Kaerlev <matpow2@gmail.com>
2014-01-24 00:25:39 +0100

    Add genchunk.py, add helper functions to tgen.pyx (is_solid, etc.)

--- cuwo/tgen.pyx
+++ cuwo/tgen.pyx
@@ -25,7 +25,7 @@ cdef extern from "terraingen.h":
         unsigned char r, g, b, a
 
     cdef struct ChunkXY:
-        int a, b
+        int a, b # b is not actually used
         unsigned int size
         ChunkEntry * items
 
@@ -41,6 +41,27 @@ cdef extern from "terraingen.h":
     void tgen_dump_mem(const char * filename) nogil
 
 
+# alpha:
+# - 5 bits, block type
+#   - 0: empty
+#   - 2: water
+#   - 3: also water?
+#   - 4: grass
+#   - 6: mountain, layer under grass
+#   - 8: trees
+# - 1 bit, unknown (not used in mesh renderer?)
+# - 1 bit, used in mesh renderer (caves, darkness? should be empty)
+
+
+cdef enum:
+    EMPTY_TYPE = 0
+    WATER_TYPE = 2
+    WATER2_TYPE = 3
+    GRASS_TYPE = 4
+    MOUNTAIN_TYPE = 6
+    TREE_TYPE = 8
+
+
 def initialize(seed):
     tgen_set_seed(seed)
     tgen_set_path('./data/')
@@ -53,6 +74,14 @@ def generate(x, y):
 
 def dump_mem(filename):
     tgen_dump_mem(filename)
+
+
+cdef int get_block_type(ChunkEntry * block):
+    return block.a & 0x1F
+
+
+cdef tuple get_block_tuple(ChunkEntry * block):
+    return (block.r, block.g, block.b)
 
 
 cdef class XYProxy:
@@ -71,7 +100,8 @@ cdef class XYProxy:
 
     def __getitem__(self, index):
         cdef ChunkEntry * data = &self.data.items[index]
-        return (data.r, data.g, data.b, data.a)
+        return get_block_tuple(data)
+
 
 cdef class ChunkProxy:
     cdef ChunkData * data
@@ -86,6 +116,51 @@ cdef class ChunkProxy:
     property y:
         def __get__(self):
             return self.data.y
+
+    cpdef bint get_solid(self, int x, int y, int z):
+        if x < 0 or x >= 256 or y < 0 or y >= 256 or z < 0 or z >= 256:
+            return True
+        cdef ChunkXY * data = self.get_xy(x, y)
+        if z < data.a:
+            return True
+        z -= data.a
+        if z >= <int>data.size:
+            return False
+        return get_block_type(&data.items[z]) != EMPTY_TYPE
+
+    cdef ChunkXY * get_xy(self, int x, int y):
+        return &self.data.items[x + y * 256]
+
+    cpdef bint get_neighbor_solid(self, int x, int y, int z):
+        return (self.get_solid(x - 1, y, z) and
+                self.get_solid(x + 1, y, z) and
+                self.get_solid(x, y + 1, z) and
+                self.get_solid(x, y - 1, z) and
+                self.get_solid(x, y, z + 1) and
+                self.get_solid(x, y, z - 1))
+
+    def get_dict(self):
+        cdef dict blocks = {}
+
+        cdef int x, y, i, n, z
+        cdef ChunkXY * xy
+        cdef ChunkEntry * block
+        for i in range(256*256):
+            x = i % 256
+            y = i / 256
+            xy = &self.data.items[i]
+            for z in range(0, <int>(xy.a + xy.size)):
+                if self.get_neighbor_solid(x, y, z):
+                    continue
+                if z < xy.a:
+                    blocks[(x, y, z)] = get_block_tuple(&xy.items[0])
+                    continue
+                block = &xy.items[z - xy.a]
+                if get_block_type(block) == EMPTY_TYPE:
+                    continue
+                blocks[(x, y, z)] = get_block_tuple(block)
+
+        return blocks
 
     def __getitem__(self, index):
         cdef XYProxy proxy = XYProxy()
---
+++ tools/genchunk.py
@@ -0,0 +1,85 @@
+# Copyright (c) Mathias Kaerlev 2013.
+#
+# This file is part of cuwo.
+#
+# cuwo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# cuwo is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with cuwo.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+sys.path.append('.')
+from cuwo import tgen
+from cuwo.bytes import ByteReader, ByteWriter
+from cuwo.qmo import QubicleFile, QubicleModel
+import argparse
+
+
+def switch_axes(x, y, z):
+    return x, z, y
+
+
+def convert_qmo(data, path):
+    qmo_file = QubicleFile()
+    qmo_model = QubicleModel()
+    x_size, y_size, z_size = 256, 256, 256
+    qmo_model.x_size = x_size
+    qmo_model.y_size = y_size
+    qmo_model.z_size = z_size
+    qmo_model.x_offset = -x_size / 2
+    qmo_model.y_offset = 0
+    qmo_model.z_offset = -z_size / 2
+
+    blocks = data.get_dict()
+    for pos, color in blocks.iteritems():
+        pos = switch_axes(*pos)
+        qmo_model.blocks[pos] = color
+
+    qmo_file.models.append(qmo_model)
+    writer = ByteWriter()
+    qmo_file.write(writer)
+    open(path, 'wb').write(writer.get())
+
+
+def convert_pos(x, y, f):
+    print 'Generating', x, y
+    data = tgen.generate(x, y)
+    print 'Converting...'
+    convert_qmo(data, f)
+
+
+def coords(s):
+    try:
+        x, y = map(int, s.split(','))
+        return x, y
+    except ValueError:
+        raise argparse.ArgumentTypeError("Coordinates must be x,y")
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Generates chunks using tgen')
+    parser.add_argument('seed', help='input seed', type=int)
+    parser.add_argument('coords', help='input coordinates',
+                        type=coords, nargs='+')
+
+    args = parser.parse_args()
+
+    print 'Initializing...'
+    tgen.initialize(args.seed)
+    for coord in args.coords:
+        x, y = coord
+        f = '%s_%s.qmo' % (x, y)
+        convert_pos(x, y, f)
+    print 'Done'
+
+
+if __name__ == '__main__':
+    main()
