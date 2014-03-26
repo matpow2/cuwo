0bf27fd3d4c2e3aacb3b4ba4437c8e2dc635cc55
Mathias Kaerlev <matpow2@gmail.com>
2014-03-26 13:01:05 +0100

    Provide percentage for tgen build

--- setup.py
+++ setup.py
@@ -25,6 +25,15 @@ from distutils.sysconfig import get_python_inc
 from distutils.sysconfig import customize_compiler
 import multiprocessing.pool
 from distutils import log
+from distutils.sysconfig import get_config_vars
+from distutils import spawn
+import subprocess
+
+# suppress warnings about -Wstrict-prototypes
+opt, = get_config_vars('OPT')
+if opt:
+    os.environ['OPT'] = ' '.join(flag for flag in opt.split()
+                                 if flag != '-Wstrict-prototypes')
 
 ext_modules = []
 
@@ -56,6 +65,26 @@ for name in names:
     ext_modules.append(Extension(name, ['./%s.pyx' % name.replace('.', '/')],
                                  language='c++', include_dirs=includes))
 
+
+_spawn_nt = spawn._spawn_nt
+
+def silent_spawn_nt(cmd, search_path=1, verbose=0, dry_run=0):
+    executable = cmd[0]
+    if search_path:
+        # either we find one or it stays the same
+        executable = spawn.find_executable(executable) or executable
+    if dry_run:
+        return
+    # spawn for NT requires a full path to the .exe
+    p = subprocess.Popen([executable] + cmd[1:], stdout=subprocess.PIPE)
+    p.wait()
+    rc = p.returncode
+    if rc != 0:
+        # and this reflects the command running but failing
+        raise spawn.DistutilsExecError, \
+              "command '%s' failed with exit status %d" % (cmd[0], rc)
+
+
 class build_ext(_build_ext.build_ext):
     def build_extensions(self):
         self.check_extensions_list(self.extensions)
@@ -80,19 +109,30 @@ class build_ext(_build_ext.build_ext):
         extra_args = []
         if is_msvc:
             extra_args += ['/wd4102', '/EHsc', '/MP']
+        else:
+            extra_args += ['-Wunused-label']
+
+        class compile_state:
+            index = 0
 
         # make a parallel build
         def compile_single(source):
+            p = (compile_state.index * 100) / len(sources)
+            p = '%02d' % p
+            compile_state.index += 1
+            sys.stdout.write('[%s%%] %s\n' % (p, os.path.basename(source)))
             return self.compiler.compile([source], output_dir=self.build_temp,
                                          macros=macros, include_dirs=includes,
                                          debug=self.debug,
                                          extra_postargs=extra_args)[0]
 
+        spawn._spawn_nt = silent_spawn_nt
         old = log.set_threshold(log.WARN)
         # convert to list, imap is evaluated on-demand
         pool = multiprocessing.pool.ThreadPool(multiprocessing.cpu_count())
         objects = list(pool.imap(compile_single, sources))
         log.set_threshold(old)
+        spawn._spawn_nt = _spawn_nt
 
         if os.name == 'nt':
             rsp_path = os.path.join(self.build_temp, 'link.rsp')
