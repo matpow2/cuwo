59cdf805f65af59b096fd2dcf32dcec552ec6cf1
Mathias Kaerlev <matpow2@gmail.com>
2014-03-30 19:07:32 +0200

    Add blacklist to master server

--- master/server.py
+++ master/server.py
@@ -30,35 +30,54 @@ from twisted.internet import reactor
 import pygeoip
 import argparse
 import json
+import os
 
 
 WRITE_INTERVAL = 10
 DEFAULT_PORT = 12364
 EXPIRE_TIME = master.UPDATE_RATE * 1.5
+BLACKLIST_FILE = 'blacklist.json'
 
 
 class MasterServer(master.MasterProtocol):
-    def __init__(self, filename):
+    def __init__(self, filename, blacklist_file):
         self.filename = filename
         self.output = {}
-        self.ignore = set()
-        self.update_loop = LoopingCall(self.update)
-        self.update_loop.start(EXPIRE_TIME)
+        self.blacklist_file = blacklist_file
+        self.blacklist = set()
         self.ip_database = pygeoip.GeoIP('GeoIP.dat')
+        self.update_loop = LoopingCall(self.update)
+        self.update_loop.start(EXPIRE_TIME, now=True)
 
     def datagramReceived(self, data, addr):
         host, port = addr
-        if host in self.ignore:
+        if host in self.blacklist:
             return
         master.MasterProtocol.datagramReceived(self, data, addr)
 
     def on_bad_packet(self, addr):
         host, port = addr
-        self.ignore.add(host)
+        self.add_blacklist(host)
+
+    def add_blacklist(self, host):
+        self.blacklist.add(host)
+        self.save_blacklist()
+
+    def save_blacklist(self):
+        with open(self.blacklist_file, 'wb') as fp:
+            fp.write(json.dumps(list(self.blacklist)))
+
+    def load_blacklist(self):
+        if not os.path.isfile(self.blacklist_file):
+            self.save_blacklist() # save empty blacklist
+            return
+        with open(self.blacklist_file, 'rb') as fp:
+            self.blacklist = set(json.loads(fp.read()))
 
     def update(self):
         with open(self.filename, 'wb') as fp:
             fp.write(json.dumps(self.output.values()))
+        self.load_blacklist()
         self.output.clear()
 
     def on_resolve(self, resolved_ip, ip, hostname, data):
@@ -107,12 +126,15 @@ def main():
     parser = argparse.ArgumentParser(description='Run a master server')
     parser.add_argument('filename', type=str)
     parser.add_argument('--port', type=int, default=DEFAULT_PORT)
+    parser.add_argument('--blacklist', type=str,
+                        default=BLACKLIST_FILE)
     args = parser.parse_args()
 
     port = args.port
     filename = args.filename
+    blacklist_file = args.blacklist
 
-    server = MasterServer(filename)
+    server = MasterServer(filename, blacklist_file)
     port = reactor.listenUDP(port, server)
     print 'Running cuwo (master) on port %s' % port.port
     reactor.run()
