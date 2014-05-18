0b430d93ae5ca4e4ad5f2321cd15208056514677
Mathias Kaerlev <matpow2@gmail.com>
2014-05-18 02:57:09 +0200

    Add set_clock and set_clock_resolution methods to the Windows event loop

--- cuwo/win32.py
+++ cuwo/win32.py
@@ -26,36 +26,72 @@ https://groups.google.com/forum/?fromgroups#!topic/python-tulip/yUFm29WPt88
 """
 
 from ctypes import windll
+from asyncio import windows_events
+
 timeGetTime = windll.winmm.timeGetTime
 timeBeginPeriod = windll.winmm.timeBeginPeriod
 timeEndPeriod = windll.winmm.timeEndPeriod
-
-from asyncio import windows_events
+TIMERR_NOERROR = 0
 
 
 def fix_event_loop(klass):
     class EventLoop(klass):
         def __init__(self):
             super().__init__()
-            # 10 ms resolution
-            self._clock_resolution = 10 * 1e-3
-            self._last_time = 0
-            self._n_overflow = 0
 
-        def run_forever(self):
-            period = int(self._clock_resolution * 1e3)
-            timeBeginPeriod(period)
-            super().run_forever()
-            # restore timer resolution
-            timeEndPeriod(period)
+            # Ensure that the scheduler never tries to fire a call in the
+            # future, so use the highest timer resolution of 1ms as the clock
+            # resolution used for scheduling
+            self._clock_resolution = 1 * 1e-3
+            self._use_default_clock = True
+            self._time_period = None
+            self._last_time = 0
+            self._time_overflow = 0
+
+        def _set_time_period(self, period):
+            self._restore_time_period()
+            self._time_period = period
+            return timeBeginPeriod(period) == TIMERR_NOERROR
+
+        def _restore_time_period(self):
+            if self._time_period is None:
+                return
+            timeEndPeriod(self._time_period)
+            self._time_period = None
+
+        def set_clock_resolution(self, clock_resolution):
+            """Sets the internal clock resolution.
+
+            Returns False if the clock resolution is not supported.
+            """
+            if not self._use_default_clock:
+                return False
+            period = max(1, int(clock_resolution * 1e3))
+            return self._set_time_period(period)
+
+        def set_clock(self, time_func, clock_resolution):
+            """Sets the internal clock implementation.
+
+            Returns False if the event loop does not support changes to its
+            clock.
+            """
+            self.time = time_func
+            self._clock_resolution = clock_resolution
+            self._use_default_clock = False
+            self._restore_time_period()
+            return True
+
+        def close(self):
+            self._restore_time_period()
+            super().close()
 
         def time(self):
             # lifted from timemodule.c (pymonotonic)
             t = timeGetTime()
             if t < self._last_time:
-                self._n_overflow += 0x100000000
+                self._time_overflow += 0x100000000
             self._last_time = t
-            return (t + self._n_overflow) * 1e-3
+            return (t + self._time_overflow) * 1e-3
 
     return EventLoop
 
