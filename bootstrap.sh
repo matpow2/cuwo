2c770237ebbf2f294a56be215e315f5d3b83a51d
Mathias Kaerlev <matpow2@gmail.com>
2014-05-19 17:18:46 +0200

    Fix bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,30 +7,21 @@ curl -o ./data/Server.exe http://mp2.dk/Server.exe
 curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
 
 # create run_pyenv.sh
-echo "#!/usr/bin/env bash" > run_pyenv.sh
-echo -e "\n\n" >> run_pyenv.sh
-echo "export PYENV_ROOT=${HOME}/.pyenv" >> run_pyenv.sh
-echo -e "\n\n" >> run_pyenv.sh
-echo "if [ -d \"${PYENV_ROOT}\" ]; then" >> run_pyenv.sh
-echo -e "\n" >> run_pyenv.sh
-echo "  export PATH=\"${PYENV_ROOT}/bin:${PATH}\"" >> run_pyenv.sh
-echo -e "\n" >> run_pyenv.sh
-echo "  eval \"$(pyenv init -)\"" >> run_pyenv.sh
-echo -e "\n" >> run_pyenv.sh
-echo "fi" >> run_pyenv.sh
+echo '#!/usr/bin/env bash' > run_pyenv.sh
+echo 'export PYENV_ROOT=${HOME}/.pyenv' >> run_pyenv.sh
+echo 'if [ -d "${PYENV_ROOT}" ]; then' >> run_pyenv.sh
+echo '  export PATH="${PYENV_ROOT}/bin:${PATH}"' >> run_pyenv.sh
+echo '  eval "$(pyenv init -)"' >> run_pyenv.sh
+echo 'fi' >> run_pyenv.sh
 
 # create update.sh
-echo "#!/usr/bin/env bash" > update.sh
-echo -e "\n\n" >> update.sh
-echo "cd \"$(dirname \"$0\")\""
-echo -e "\n" >> update.sh
-echo "sh run_pyenv.sh"
-echo -e "\n" >> update.sh
-echo "git pull" >> update.sh
-echo -e "\n" >> update.sh
-echo "python setup.py build_ext --inplace" >> update.sh
+echo '#!/usr/bin/env bash' > update.sh
+echo 'cd "$(dirname "$0")"'
+echo 'source run_pyenv.sh'
+echo 'git pull' >> update.sh
+echo 'python setup.py build_ext --inplace' >> update.sh
 
-sh run_pyenv.sh
+source run_pyenv.sh
 
 pyenv update
 pyenv install 3.4.0 --skip-existing
