f5f51ca1e5debea5488c5449cee32e3591532967
Mathias Kaerlev <matpow2@gmail.com>
2017-07-04 00:54:16 +0200

    Add testtgen.py

---
+++ tools/testtgen.py
@@ -0,0 +1,22 @@
+# Copyright (c) Mathias Kaerlev 2013-2014.
+#
+# This file is part of cuwo.
+#
+# cuwo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# cuwo is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with cuwo.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+sys.path.append('.')
+from cuwo import tgen
+tgen.initialize(123456, './data/')
+tgen.generate(5000, 5000)
