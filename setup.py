b3af824bd52d14220273d9597b55d93a258b5ae2
Mathias Kaerlev <matpow2@gmail.com>
2017-07-23 17:10:51 +0200

    Oops

--- setup.py
+++ setup.py
@@ -15,9 +15,9 @@
 # You should have received a copy of the GNU General Public License
 # along with cuwo.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
 import sys
 sys.path.append(os.path.dirname(__file__))
-import os
 import numpy
 from distutils.core import setup
 from distutils.extension import Extension
