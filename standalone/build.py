680485f6badf6133ecfe4a433e714f36f0c4bdbb
Mathias Kaerlev <matpow2@gmail.com>
2018-06-23 04:09:18 +0200

    Fix standalone build

--- standalone/build.py
+++ standalone/build.py
@@ -15,14 +15,29 @@
 # You should have received a copy of the GNU General Public License
 # along with cuwo.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
 import sys
 sys.path.append('..')
+
+# cx_Freeze issue 155
+PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
+os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
+os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
+
 from cx_Freeze import setup, Executable
 
-build_options = {'packages': ['encodings', 'irc3', 'cuwo', 'asyncio', 'numpy',
-                              'pyrr', 'discord'],
-                 'excludes': ['config'],
-                 'includes': ['json', 'platform']}
+build_options = {
+    'packages': ['encodings', 'irc3', 'cuwo', 'asyncio', 'numpy',
+                 'pyrr', 'discord'],
+    'excludes': ['config'],
+    'includes': ['json', 'platform'],
+
+    # cx_Freeze issue 155
+    'include_files': [
+        os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tk86t.dll'),
+        os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tcl86t.dll')
+    ]
+}
 
 executables = [
     Executable('../cuwo/server.py', base='Console', targetName='server.exe')
